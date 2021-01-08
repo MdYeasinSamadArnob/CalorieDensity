a2cba218aa268e36707222d58c5880230a443418
Patti85 <pbeier24@googlemail.com>
2021-01-08 22:28:01 +0100

    table

--- calorie-density/src/components/food-list.component.js
+++ calorie-density/src/components/food-list.component.js
@@ -4,7 +4,36 @@ export default class FoodList extends Component {
     render() {
         return (
             <div>
-                <p>Welcome to Food List Component!</p>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Amount(g)</th>
+                            <th scope="col">Item</th>
+                            <th scope="col">kcal/100g</th>
+                            <th scope="col">Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>12</td>
+                            <td>Banana</td>
+                            <td>1</td>
+                            <td>good</td>
+                        </tr>
+                        <tr>
+                            <td>233</td>
+                            <td>Berries</td>
+                            <td>2.3</td>
+                            <td>okay</td>
+                        </tr>
+                        <tr>
+                            <td>55</td>
+                            <td>Bread</td>
+                            <td>6</td>
+                            <td>bad</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
