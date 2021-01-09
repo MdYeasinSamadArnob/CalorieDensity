5777bd122d1a854b3d06a4ea778e6af4a2dc9205
Patti85 <pbeier24@googlemail.com>
2021-01-09 22:16:19 +0100

    get table row info onclick

--- calorie-density/src/components/searchresults.component.js
+++ calorie-density/src/components/searchresults.component.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchResults = (props) => {
   const options = props.results.map(r => (
-    <tr key={r._id}>
+    <tr key={r._id}  onClick={()=> alert(r.fields.nf_calories)}>
         <td>
         {r.fields.item_name}
         </td>
