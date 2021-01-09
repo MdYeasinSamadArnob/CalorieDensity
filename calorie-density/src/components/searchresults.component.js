6aeac831127b25782663b8ef296d3bc6f9eb52af
Patti85 <pbeier24@googlemail.com>
2021-01-09 23:44:16 +0100

    input in gram

--- calorie-density/src/components/searchresults.component.js
+++ calorie-density/src/components/searchresults.component.js
@@ -1,21 +1,27 @@
 import React from 'react'
 
 const SearchResults = (props) => {
-  const options = props.results.map(r => (
-    <tr key={r._id}  onClick={()=> alert(r.fields.nf_calories)}>
-        <td>
-        {r.fields.item_name}
-        </td>
-        <td>
-        {r.fields.brand_name}
-        </td>   
-        <td>
-        {r.fields.nf_calories}
-        </td>   
-   </tr>
- ))
-  return <table>{options}</table>
- 
+    const options = props.results.map(r => (
+        <tr key={r._id} onClick={() => {
+            var test = prompt("Please enter consumed " + r.fields.item_name
+            + " amount in g!");
+            alert(test);
+        }
+        }
+        >
+            <td>
+                {r.fields.item_name}
+            </td>
+            <td>
+                {r.fields.brand_name}
+            </td>
+            <td>
+                {r.fields.nf_calories}
+            </td>
+        </tr>
+    ))
+    return <table>{options}</table>
+
 }
 
 export default SearchResults
